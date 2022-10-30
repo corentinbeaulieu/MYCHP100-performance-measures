a760b324ad9447c5e100c4c3d937bb7a640d2e14
corentinbeaulieu <corentin.beaulieu@ens.uvsq.fr>
2022-10-30 11:38:32 +0100

    Add the unroll functions to kernels files
    The reduc has now functions with the loop unrolled with a factor 4 8 and
    16.

--- reduc/kernels.c
+++ reduc/kernels.c
@@ -19,3 +19,88 @@ f64 reduc_base(f64 *restrict a, u64 n)
   return d;
 }
 
+// Unroll the loop with a factor of 4
+f64 reduc_unroll4 (f64 *restrict a, u64 n)
+{
+  #define UNROLL4 4
+
+  double d = 0.0;
+  u64 i = 0;
+
+  for (i = 0; i < n - UNROLL4; i += UNROLL4) {
+    d += a[i];
+    d += a[i+1];
+    d += a[i+2];
+    d += a[i+3];
+  }
+
+  while(i < n) {
+    d += a[i];
+    i++;
+  }
+
+  return d;
+}
+
+// Unroll the loop with a factor of 8
+f64 reduc_unroll8 (f64 *restrict a, u64 n)
+{
+  #define UNROLL8 8
+
+  double d = 0.0;
+  u64 i = 0;
+
+  for (i = 0; i < n - UNROLL8; i += UNROLL8) {
+    d += a[i];
+    d += a[i+1];
+    d += a[i+2];
+    d += a[i+3];
+    d += a[i+4];
+    d += a[i+5];
+    d += a[i+6];
+    d += a[i+7];
+  }
+
+  while(i < n) {
+    d += a[i];
+    i++;
+  }
+
+  return d;
+}
+
+// Unroll the loop with a factor of 16 
+f64 reduc_unroll16 (f64 *restrict a, u64 n)
+{
+  #define UNROLL16 16 
+
+  double d = 0.0;
+  u64 i = 0;
+
+  for (i = 0; i < n - UNROLL16; i += UNROLL16) {
+    d += a[i];
+    d += a[i+1];
+    d += a[i+2];
+    d += a[i+3];
+    d += a[i+4];
+    d += a[i+5];
+    d += a[i+6];
+    d += a[i+7];
+    d += a[i+8];
+    d += a[i+9];
+    d += a[i+10];
+    d += a[i+11];
+    d += a[i+12];
+    d += a[i+13];
+    d += a[i+14];
+    d += a[i+15];
+  }
+
+  while(i < n) {
+    d += a[i];
+    i++;
+  }
+
+  return d;
+}
+
