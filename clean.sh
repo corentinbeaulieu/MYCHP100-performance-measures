4b53d1a1ee9b8f382cad37250e5234d2e74a8527
corentinbeaulieu <corentin.beaulieu@ens.uvsq.fr>
2022-11-06 17:08:03 +0100

    Add multiplot
    The perf.sh now creates a multiplot of the version by compiler too.
    The clean.sh file has been updated to remove the multiplot scripts
    generated by the execution of perf.sh.

--- clean.sh
+++ clean.sh
@@ -5,7 +5,10 @@ BIN_DIR='dgemm dotprod reduc'
 
 for actual_dir in $BIN_DIR
 do
-    rm -f plot_$actual_dir.gp
+    # Delete gnuplot scripts
+    rm -f plot-$actual_dir.gp
+    rm -f multiplot-$actual_dir.gp
+    # Delete executable and .dat files
     cd $actual_dir
     make clean
     cd ..
--- perf.sh
+++ perf.sh
@@ -1,39 +1,47 @@
 
 #!/bin/bash
 
-# Définition des variables
+# Variable definition
 SCR_DIR=Scripts
 BIN_DIR="dgemm dotprod reduc"
 RES_DIR=RESULTS
-CC='gcc clang' # icx
+CC_potential='gcc clang icc icx'
 OFLAGS='-O0 -O1 -O2 -O3 -Ofast'
 PLOT_BASE=$SCR_DIR/plot-base.gp
-N=100
-R=10
+N=128
+R=33
 
-# preparing Results repository
+# Preparing Results repository
 rm -rf $RES_DIR
 mkdir $RES_DIR
 
 # Get computer information
 RES_DIR=$RES_DIR $SCR_DIR/arch.sh 
 
-# dgemm
+# Verify compilers available
+compiler=0
+CC=""
 
+for cc in $CC_potential
+do
+  $cc --version 1>/dev/null 2>/dev/null
+  if [[ $? -eq 0 ]]
+  then
+    CC=$CC" "$cc
+    compiler=1
+  fi
+done
+
+if [[ $compiler -eq 0 ]]
+then
+    echo -e "No suitable compiler found\nPlease get one of those : $CC_potential"
+else
+
+# Main loop
 for actual_dir in $BIN_DIR
 do
-  # preparing gnuplot script
-  plot_scr=plot_$actual_dir.gp
-  rm -f $plot_scr
-
-  cp $PLOT_BASE $plot_scr
-  echo -e "set title \"Performance $actual_dir n=$N en fonction de l'implémentation \
-et des options de compilation\"" >> $plot_scr
-  echo -e "set output \"$RES_DIR/plot-$actual_dir$N.png\"" >> $plot_scr
-
-  printf "plot " >> $plot_scr
-  
-  # proceed benchmarks
+ 
+  # Proceed benchmarks
   cd $actual_dir 
   make clean
 
@@ -43,21 +51,70 @@ et des options de compilation\"" >> $plot_scr
     do
        touch main.c
        make CC=$cc OFLAGS=$oflag 
-       printf "RUNNING %7s %5s %6s\n" $actual_dir $cc $oflag
+       printf "RUNNING %7s %5s %6s benchmark\n" $actual_dir $cc $oflag
        taskset -c 1 ./$actual_dir $N $R
+       i=0
        for data_file in $( ls | grep .dat )
        do
          printf "%5s %6s\n" $cc $oflag >> $data_file
+         i=$(( $i + 1 ))
        done
     done
   done
 
-  # plot the data obtained
+  # Plot the data obtained
   cd ..
+  # Preparing gnuplot script
+  plot_scr=plot-$actual_dir.gp
+  rm -f $plot_scr
+
+  cp $PLOT_BASE $plot_scr
+  echo -e "set terminal png size 1200,700" >> $plot_scr
+  echo -e "set output \"$RES_DIR/plot-$actual_dir$N.png\"\n" >> $plot_scr
+
+  printf "plot " >> $plot_scr
   for data_file in $( ls $actual_dir | grep .dat )
   do
       echo -e "\"$actual_dir/$data_file\" u 11:(\$10/\$9*\$11):xtic(12)\
  t \"$( echo $data_file | cut -d '.' -f 1 )\", \\" >> $plot_scr
   done
   gnuplot $plot_scr
+  echo "PLOT GENERATED : $RES_DIR/plot-$actual_dir$N.png"
+  
+  # Multiplot the data obtained
+  # Preparing gnuplot script
+  plot_scr=multiplot-$actual_dir.gp
+  rm -f $plot_scr
+
+  cp $PLOT_BASE $plot_scr
+  echo -e "set terminal png size 1200,1000" >> $plot_scr
+  echo -e "set output \"$RES_DIR/multiplot-$actual_dir$N.png\"\n" >> $plot_scr
+
+  echo "set multiplot layout $(($i/2 + $i%2)),2 rowsfirst" >> $plot_scr
+  echo "unset xlabel" >> $plot_scr
+  i=1
+  for data_file in $( ls $actual_dir | grep .dat )
+  do
+      # Show ylabel only for the first column
+      if [[ $(( $i%2 )) -eq 1 ]]
+      then
+          echo -e "set ylabel \"Mio/s\"" >> $plot_scr
+      else
+          echo -e "unset ylabel" >> $plot_scr
+      fi
+      # Set title to each graph
+      echo -e "set title \"$( echo $data_file | cut -d '.' -f 1 )\"" >> $plot_scr
+      echo -e "plot \"$actual_dir/$data_file\" u 11:(\$10/\$9*\$11):xtic(12) notitle lc \"grey20\"" >> $plot_scr
+      i=$(( i + 1 ))
+  done
+  echo "unset multiplot" >> $plot_scr
+  gnuplot $plot_scr
+  echo "PLOT GENERATED : $RES_DIR/multiplot-$actual_dir$N.png"
+
+  # Set N for the dotprod and reduc
+  N=$(( 2 ** 20 ))
 done
+fi
+
+# Clean all temporary files (.dat .gp)
+./clean.sh
